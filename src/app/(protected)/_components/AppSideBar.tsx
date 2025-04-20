55f6f2d5ca3f718de8768af96ee88b2dffac7a82
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-04-20 12:59:20 +0530

    Basic Routing Added

--- src/app/(protected)/_components/AppSideBar.tsx
+++ src/app/(protected)/_components/AppSideBar.tsx
@@ -13,7 +13,6 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
-// import useProject from "@/hooks/use-project";
 import { cn } from "@/lib/utils";
 import {
   Bot,
@@ -51,13 +50,25 @@ const items = [
   },
 ];
 
+const project = [
+  {
+    name: "project 1",
+    url: "/projects/project-1",
+  },
+  {
+    name: "project 2",
+    url: "/projects/project-2",
+  },
+  {
+    name: "project 3",
+    url: "/projects/project-3",
+  },
+];
 
 const AppSidebar = ({}: Props) => {
   const pathname = usePathname();
-
-//   const {projects, projectId, setProjectId} = useProject();
-
   const { open } = useSidebar();
+
   return (
     <Sidebar collapsible="icon" variant="floating">
       <SidebarHeader>
@@ -73,23 +84,21 @@ const AppSidebar = ({}: Props) => {
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => {
-                return (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <Link
-                        href={item.url}
-                        className={cn({
-                          "!bg-primary !text-white": pathname === item.url,
-                        })}
-                      >
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                );
-              })}
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={item.url}
+                      className={cn({
+                        "!bg-primary !text-white": pathname === item.url,
+                      })}
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
@@ -97,42 +106,35 @@ const AppSidebar = ({}: Props) => {
         <SidebarGroup>
           <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
           <SidebarGroupContent>
-            {/* <SidebarMenu>
-              {projects?.map((project) => {
-                return (
-                  <SidebarMenuItem key={project.name}>
-                    <SidebarMenuButton asChild>
-                      <div onClick={() => setProjectId(project.id)}>
-                        <div
-                          className={cn(
-                            "flex size-6 items-center justify-center rounded-sm border bg-white text-sm text-primary",
-                            {
-                              "bg-primary text-white": project.id === projectId,
-                            },
-                          )}
-                        >
-                          {project.name[0]}
-                        </div>
-                        <span>{project.name}</span>
-                      </div>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                );
-              })}
+            <SidebarMenu>
+              {project.map((proj) => (
+                <SidebarMenuItem key={proj.name}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={proj.url}
+                      className={cn({
+                        "!bg-primary !text-white": pathname === proj.url,
+                      })}
+                    >
+                      <span>{proj.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
 
               <div className="h-2"></div>
 
               {open && (
                 <SidebarMenuItem>
                   <Link href="/create">
-                    <Button size="sm" variant={"outline"} className="w-fit">
-                      <Plus />
+                    <Button size="sm" variant="outline" className="w-fit">
+                      <Plus className="mr-1 h-4 w-4" />
                       Create Project
                     </Button>
                   </Link>
                 </SidebarMenuItem>
               )}
-            </SidebarMenu> */}
+            </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
