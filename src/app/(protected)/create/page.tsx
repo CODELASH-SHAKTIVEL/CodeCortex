1ea6fdb4366db2c9de811d2c61178b91c188abc9
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2025-05-02 20:40:48 +0530

    stripe test mode has been integrated

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,86 +1,137 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import useRefetch from '@/hooks/use-refetch'
-import { api } from '@/trpc/react'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
+"use client";
 
-type FormInput = {
-  repoUrl: string,
-  projectName: string,
-  githubToken?: string,
-}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import useRefetch from "@/hooks/use-refetch";
+import { api } from "@/trpc/react";
+import { type FormInput } from "@/types/FormInput";
+import { FileWarning, Info } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
+type Props = {};
 
-const CreateProjectPage = () => {
-  const { register, handleSubmit, reset } = useForm<FormInput>()
-  const createProject = api.project.createProject.useMutation()
-  const refetch = useRefetch()
+const page = ({}: Props) => {
+  const { register, handleSubmit, reset } = useForm<FormInput>();
+  const createProject = api.project.createProject.useMutation();
+  const checkCredits = api.project.checkCredits.useMutation();
+
+  const refetch = useRefetch();
 
   function onSubmit(data: FormInput) {
-    createProject.mutate({
-      repoUrl: data.repoUrl,
-      projectName: data.projectName,
-      githubToken: data.githubToken,
-    }, {
-      onSuccess: () => {
-        toast.success('Project created successfully')
-        refetch()
-      },
-      onError: () => {
-        toast.error('Error creating project:')
-      }
-    })
-    console.log(data)
-    reset();
+    if (!!checkCredits.data) {
+      createProject.mutate(
+        {
+          repoUrl: data.repoUrl,
+          projectName: data.projectName,
+          githubToken: data.githubToken,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Project created successfully");
+            refetch();
+            reset();
+          },
+          onError: () => {
+            toast.error("Failed to create project");
+          },
+        },
+      );
+    } else {
+      checkCredits.mutate({
+        githubUrl: data.repoUrl,
+        githubToken: data.githubToken,
+      });
+    }
+
+    return true;
   }
 
+  const hasEnoughCredits = checkCredits?.data?.userCredits
+    ? checkCredits.data.fileCount <= checkCredits.data.userCredits
+    : true;
   return (
-    <div className='flex items-center gap-12 h-full justify-center'>
-      <img src={'/logo.svg'} className='h-56 w-auto' />
+    <div className="flex h-full items-center justify-center gap-12">
+      <img src="/logo.svg" className="h-56 w-auto" />
       <div>
         <div>
-          <h1 className='font-semibold text-2xl'>
-            Link your repository to get started.
+          <h1 className="text-2xl font-semibold">
+            Link your Github Repository
           </h1>
-          <p className='text-sm text-muted-foreground'>
-            Enter your repository URL and project name.
+          <p className="text-sm text-muted-foreground">
+            Enter the URL of your repository to link it to Dionysus
           </p>
-          <div className="h-4"></div>
-          <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Input
-                {...register('projectName', { required: true })}
-                placeholder='Project Name'
-                required
-              >
-              </Input>
-              <div className="h-2"></div>
-              <Input
-                {...register('repoUrl', { required: true })}
-                placeholder='Repository URL'
-                type='url'
-                required
-              >
-              </Input>
-              <div className="h-2"></div>
-              <Input
-                {...register('githubToken')}
-                placeholder='Github Token (Optional)'
-              />
-              <div className="h-4"></div>
-              <Button type='submit' disabled={createProject.isPending}>
-                Create Project
-              </Button>
-            </form>
+        </div>
+
+        <div className="h4"></div>
+
+        <div>
+          <div className="mt-4 rounded-md border border-red-200 bg-red-50 px-4 py-2 mb-2 text-red-700">
+            <div className="flex items-center gap-2">
+              <FileWarning className="size-4" />
+              <p className="text-sm">
+                It is suggested to link repository with {" "}
+                <strong> less than 50 files</strong> for best experience.
+              </p>
+            </div>
           </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("projectName", { required: true })}
+              placeholder="Project Name"
+              required
+            />
+
+            <div className="h-2"></div>
+
+            <Input
+              {...register("repoUrl", { required: true })}
+              placeholder="Github URL"
+              type="url"
+              required
+            />
+
+            <div className="h-2"></div>
+
+            <Input
+              {...register("githubToken")}
+              placeholder="Github Token (optional)"
+            />
+
+            {!!checkCredits.data && (
+              <div className="mt-4 rounded-md border border-orange-200 bg-orange-50 px-4 py-2 text-orange-700">
+                <div className="flex items-center gap-2">
+                  <Info className="size-4" />
+                  <p className="text-sm">
+                    You will be charged{" "}
+                    <strong>{checkCredits.data?.fileCount}</strong> credits for
+                    this repository.
+                  </p>
+                </div>
+                <p className="ml-6 text-sm text-blue-600">
+                  You have <strong>{checkCredits.data?.userCredits}</strong>{" "}
+                  credits remaining.
+                </p>
+              </div>
+            )}
+
+            <div className="h-4"></div>
+
+            <Button
+              type="submit"
+              disabled={
+                createProject.isPending ||
+                checkCredits.isPending ||
+                !hasEnoughCredits
+              }
+            >
+              {!!checkCredits.data ? "Create Project" : "Check Credits"}
+            </Button>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateProjectPage;
-
+export default page;
---
+++ src/lib/stripe.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-04-30.basil",
+});
+
+export async function createCheckoutSession(credits: number) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    throw new Error("Unauthorized!");
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: `${credits} Dionysus Credits`,
+          },
+          unit_amount: Math.round((credits / 50) * 75 * 100),
+        },
+        quantity: 1,
+      },
+    ],
+    customer_creation: "always",
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/create`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/billing`,
+    client_reference_id: user.id,
+    metadata: {
+      credits,
+    },
+  });
+
+  return redirect(session.url!);
+}
